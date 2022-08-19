7ca6d67b9d342bc73e61df54f04af3b43ca23361
Rishi Srivastava <official.rishisrivastava@example.com>
2022-08-20 00:03:27 +0530

    all necessary dependencies added

---
+++ app/src/main/java/com/rsstudio/tweeky/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rsstudio.tweeky.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rsstudio.tweeky.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
